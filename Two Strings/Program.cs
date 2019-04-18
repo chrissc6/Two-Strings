7207e83c735f5f64e718d23aaf9167517c0e6b4d
chris <christopherscaudill@gmail.com>
2019-04-18 04:00:19 -0400

    set

--- Two Strings/Program.cs
+++ Two Strings/Program.cs
@@ -7,6 +7,38 @@ namespace Two_Strings
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string s1 = "hello";
+            string s2 = "world";
+            string x = twoStrings(s1, s2);
+            Console.WriteLine(x);
+
+            s1 = "hi";
+            s2 = "workd";
+            x = twoStrings(s1, s2);
+            Console.WriteLine(x);
+        }
+
+        static string twoStrings(string s1, string s2)
+        {
+            string r = "NO";
+
+            return r;
         }
     }
 }
+
+/*
+Given two strings, determine if they share a common substring. A substring may be as small as one character.
+
+For example, the words "a", "and", "art" share the common substring (a).
+The words "be" and "cat" do not share a substring. 
+
+-----
+
+Complete the function twoStrings in the editor below. It should return a string, 
+either YES or NO based on whether the strings share a common substring.
+
+twoStrings has the following parameters:
+s1, s2: two strings to analyze .
+
+*/
