0b2f2e92f1338793709fa64a2f06b06614107e96
chris <christopherscaudill@gmail.com>
2019-04-18 03:50:42 -0400

    Add project files.

---
+++ Two Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Two_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
